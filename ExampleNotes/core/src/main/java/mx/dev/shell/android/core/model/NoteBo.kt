2c941879ac89571505775b12728c4e6c918765a8
Cesar Morales Garduño <shell.one.core@gmail.com>
2022-04-24 15:58:22 -0500

    Creación de módulo Core

---
+++ ExampleNotes/core/src/main/java/mx/dev/shell/android/core/model/NoteBo.kt
@@ -0,0 +1,8 @@
+package mx.dev.shell.android.core.model
+
+data class NoteBo(
+    var id: Int? = null,
+    var creationDate: Long? = null,
+    var title: String? = null,
+    var content: String? = null
+)
